83832145c0816e4f68f8619dad5a6e800c1ae637
fernando.liborio@opet.edu.br <fernando.liborio@opet.edu.br>
2018-06-05 01:19:47 -0300

    https://github.com/FernandoIbaePLiborio/data-table-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DataTableComponent } from './data-table/data-table.component';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { AppComponent } from './app.component';
+import { Routes } from '@angular/router'
+
+export const ROUTES: Routes = [
+  { path: '', component: AppComponent },
+]
+  
